40e30970fac3dcfe6fca1fdf2d5b4cd65095d7c4
kurmasov <kurmasov-sh@ugraweb.ru>
2023-11-01 17:52:22 +0300

    запуск сервера, подключение к бд

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 console.log('123')
 import express  from "express"
+import mysql  from "mysql"
 
 const PORT = 5000;
 
 const app = express()
 
 app.listen(PORT, () => console.log('SERVER STARTED' + PORT))
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database: "shop_db",
+  password : '',
+});
+
+connection.connect();
+let query = 'SELECT * FROM products'
+connection.query(query, (err, result) => {
+    console.log(err);
+    console.log(result);
+});
+
